7db34866c4b6da76fe10e988da695506a9fdd0a4
dbolotov <dmitriy.bolotov@softwareag.com>
2021-07-21 19:06:19 -0700

    added export tag to add_two_numbers.R

--- R/add_two_numbers.R
+++ R/add_two_numbers.R
@@ -2,10 +2,14 @@
 #'
 #' @param x A number.
 #' @param y A number.
+#'
 #' @return The sum of \code{x} and \code{y}.
+#'
 #' @examples
 #' add_two_numbers(1, 1)
 #' add_two_numbers(10, 1)
+#'
+#' @export
 add_two_numbers <- function(x, y) {
   x + y
 }
