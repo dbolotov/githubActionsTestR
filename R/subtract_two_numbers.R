aca60c04ccab1f141db963bd2c1543c51e7c5ef5
dbolotov <dmitriy.bolotov@softwareag.com>
2021-07-26 11:55:10 -0700

    added the function subtract_two_numbers.R

---
+++ R/subtract_two_numbers.R
@@ -0,0 +1,15 @@
+#' Subtract two numbers
+#'
+#' @param x A number.
+#' @param y A number.
+#'
+#' @return The subtraction of \code{y} from \code{x}.
+#'
+#' @examples
+#' subtract_two_numbers(1, 1)
+#' subtract_two_numbers(10, 1)
+#'
+#' @export
+subtract_two_numbers <- function(x, y) {
+  x - y
+}
